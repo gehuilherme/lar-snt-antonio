f62b4e3620495861d27acf373c7a0171da0356c5
Guilherme Henrique <32115850+gehuilherme@users.noreply.github.com>
2018-11-25 17:07:33 -0200

    Delete DatabaseConnection.php

--- cad-pac/DatabaseConnection.php
+++
@@ -1,8 +0,0 @@
-<meta charset="utf-8">
-<?php
-	$con=mysqli_connect("localhost","root","","pjenfermagem");
-	if (mysqli_connect_errno())
-	    {
-	    	echo "Failed to connect to MySQL: " . mysqli_connect_error();
-	    }
-?>
